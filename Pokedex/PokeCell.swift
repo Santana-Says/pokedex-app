7d6b3683ce082a203fe4c8b5602f3ffca8b9bab0
Jeffrey Santana <Santana@jeffreys-mini.attlocal.net>
2017-09-24 14:46:07 -0400

    connected the collectionview to display all 718 pokemon

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Jeffrey Santana on 9/24/17.
+//  Copyright © 2017 Jeffrey Santana. All rights reserved.
+//
+
+import Cocoa
+
+class PokeCell: UICollectionViewCell {
+
+}
---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Jeffrey Santana on 9/24/17.
+//  Copyright © 2017 Jeffrey Santana. All rights reserved.
+//
+
+import Foundation
